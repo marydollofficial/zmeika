6227e7853d7b5d30168e8dad9f3765bcaf665a27
marydollofficial <mtuboltseva@mail.ru>
2021-10-21 23:06:50 +0300

    Создание змейки

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+
+pygame.init()
+dis = pygame.display.set_mode((400, 300))
+
+pygame.display.set_caption('Змейка Маши')
+
+black = (0, 0, 255)
+red = (255, 0, 0)
+
+game_over = False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+    pygame.draw.rect(dis, red, [200, 150, 10, 10])
+    pygame.display.update()
+pygame.quit()
+quit()
